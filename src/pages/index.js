ff16c455fdf72b345302ff701e01f77aae3403ed
Olamilekan Rasaq Aremu <aremulekan@gmail.com>
2021-10-20 16:28:55 +0300

    added open graph protocol metadata for LinkedIn parsing

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,18 @@ const Home = () => {
           name="description"
           content="Olamilekan Aremu's Portfolio Website"
         />
-
+        <meta
+          name="title"
+          property="og:title"
+          content="Olamilekan Aremu's Portfolio Website"
+        />
+        <meta
+          name="description"
+          property="og:description"
+          content="Olamilekan Aremu's Portfolio Website"
+        />
         <meta name="keywords" content="Software Developer, Olamilekan Aremu" />
-
+        <meta name="image" property="og:image" content="/images/og.png" />
         <meta name="author" content="Olamilekan Aremu" />
       </Head>
       <Section grid>
